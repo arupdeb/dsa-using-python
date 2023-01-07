713074dbd5a8aa91a873b4a7c04d96ae168fbd8d
arupdeb <arup.goku.deb@gmail.com>
2023-01-08 01:08:54 +0530

    valid parantheses : solution 2

--- Stacks/valid_parantheses.py
+++ Stacks/valid_parantheses.py
@@ -31,6 +31,42 @@ s consists of parentheses only '()[]{}'.
 
 # solution 1: using a stack
 # time complexity : o(n) && space complexity: o(n) // worst case scenario: store all input brackets into the stack
+# runtime: 14 ms ; space required is more
+# class Solution(object):
+#     def isValid(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         #print(s)
+#         openBrackets = ["(","{","["]
+#         closeBrackets = [")", "}", "]"]
+#         stack = []
+#         if len(s)%2 != 0 :
+#             return False
+#         else:
+#             for i  in range(len(s)):
+#                 if s[i] in openBrackets:
+#                     stack.append(s[i])
+#                     # print(stack,i)
+#                 elif len(stack) != 0 and closeBrackets.index(s[i]) == openBrackets.index(stack[len(stack)-1]):
+#                     stack.pop()
+#                     # print(stack,i)
+#                 elif len(stack) == 0 and s[i] in closeBrackets:
+#                     return False
+#                 else:
+#                     return False
+
+#             if len(stack) != 0 :
+#                 # print(stack)
+#                 return False
+#             else:
+#                 return True             
+
+
+# solution 2: used hashmap instead of array to store bracket pair matcher
+# time complexity: o(n); space complexity o(n)
+# runtime : 18ms ; space required is less.
 
 class Solution(object):
     def isValid(self, s):
@@ -38,28 +74,30 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        #print(s)
-        openBrackets = ["(","{","["]
-        closeBrackets = [")", "}", "]"]
         stack = []
-        if len(s)%2 != 0 :
-            return False
-        else:
-            for i  in range(len(s)):
-                if s[i] in openBrackets:
-                    stack.append(s[i])
-                    # print(stack,i)
-                elif len(stack) != 0 and closeBrackets.index(s[i]) == openBrackets.index(stack[len(stack)-1]):
+        closingPair = { 
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        for c in s:
+            if c in closingPair:
+                if stack and stack[-1] == closingPair[c]:
                     stack.pop()
-                    # print(stack,i)
-                elif len(stack) == 0 and s[i] in closeBrackets:
-                    return False
                 else:
                     return False
-
-            if len(stack) != 0 :
-                # print(stack)
-                return False
             else:
-                return True             
-                    
+                stack.append(c)
+        
+        if not stack:
+            return True
+        else:
+            return False
+
+def main():
+    sol = Solution()
+    print(sol.isValid("[[]]{[()]}"))
+
+if __name__ == "__main__":
+    main()
